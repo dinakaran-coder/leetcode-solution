e64bd69a9df3ff21905be6be9da53e4532277dff
dinakaran-coder <dina862008karan@gmail.com>
2026-07-19 21:07:58 +0530

    Time: 53 ms (99.72%), Space: 64.2 MB (24.52%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,16 @@
+/**
+ * @param {any} object
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (obj == null || typeof classFunction != 'function' )
+        return false;
+    
+    let val = (Object(obj) instanceof classFunction);
+    return val;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
